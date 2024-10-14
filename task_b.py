bbc02020fc3c7abb691da16750752138a2f3228b
Violet-gd <nsjn6583@leeds.ac.uk>
2024-10-14 23:44:50 +0000

    task_b.py

--- task_b.py
+++ task_b.py
@@ -1,15 +1,17 @@
-grade=int(input("Please enter a number"))
-if 100>=grade>=80:
+grade=input("Please enter a number")
+if grade.isalpha():
+    print("Error: Please enter a number")
+elif 100>=int(grade)>=80:
     print("Your grade is: A")
-elif 79>=grade>=60:
+elif 79>=int(grade)>=60:
     print("Your grade is: B")
-elif 59>=grade>=50:
+elif 59>=int(grade)>=50:
     print("Your grade is: C")
-elif 49>=grade>=40:
+elif 49>=int(grade)>=40:
     print("Your grade is: D")
-elif 39>=grade>=0:
+elif 39>=int(grade)>=0:
     print("Your grade is: F")
-elif grade>100 and grade<0:
+elif int(grade)>100 or int(grade)<0:
     print("Error: Grades must be between 0 and 100")
 else:
     print("Error: Please enter a number")
