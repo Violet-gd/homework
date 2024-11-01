9f7bbf6c76442d3b8db86d311158b4e39fca1812
Violet-gd <nsjn6583@leeds.ac.uk>
2024-11-01 16:27:25 +0000

    1

--- task2/grade_analyser.py
+++ task2/grade_analyser.py
@@ -39,7 +39,7 @@ for i in grade:
      num_mod=0
      for j in i[1:]:
           if j:
-               mean+=int(j)
+               mean+=float(j)
                num_mod+=1
      mean=mean/num_mod
      if mean>=70:
